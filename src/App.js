44ddb1b4094ed2b6f5ac78cffc53843b050cd162
konstantin <k.lebedev89@gmail.com>
2020-12-05 22:55:40 +0400

    state and data in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Table from './components/Table'
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const littleDataUrl = `http://www.filltext.com/?rows=32&id={number|1000}
+  &firstName={firstName}&lastName={lastName}&email={email}
+  &phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+  const bigDataUrl = `http://www.filltext.com/?rows=1000&id={number|1000}
+  &firstName={firstName}&delay=3&lastName={lastName}&email={email}
+  &phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+
+
+  useEffect( () => {
+    const fetchData = async() => {
+      setLoading(true)
+      try {
+        await axios(littleDataUrl).then(resp => console.log(resp.data))
+      } catch(e) {
+        console.log(e)
+      }
+      setLoading(false)
+    }
+
+    fetchData()
+
+  }, [] )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      { loading ? <h1>Loading...</h1>
+      : <Table />}
     </div>
   );
 }
